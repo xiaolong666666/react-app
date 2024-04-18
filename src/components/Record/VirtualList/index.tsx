4afa9419cf8dc971c55b27ff80659e23c0ee4676
xiaolong666666 <long.1585097369@qq.com>
2024-04-18 19:55:07 +0800

    fix: ts warning

--- src/components/Record/VirtualList/index.tsx
+++ src/components/Record/VirtualList/index.tsx
@@ -3,14 +3,14 @@ import styles from "./index.module.less";
 
 const VirtualList = () => {
   // @ts-ignore
-  const list: any = [...new Array(1000).keys()].map((n) => ({
+  const list: Record<string, any>[] = [...new Array(1000).keys()].map((n) => ({
     id: n + 1,
     label: `label ${n + 1}`,
   }));
   return (
     <Card title="虚拟长列表">
       <VirtualScroller dataSource={list} pageSize="10">
-        {(item) => (
+        {(item: Record<string, any>) => (
           <div key={item.id} className={styles.item}>
             {item.label}
           </div>
